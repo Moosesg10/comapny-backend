68295bce3a78e850bb999ebc922181df57f989cb
Moosesg10 <moisesgak66@gmail.com>
2023-08-12 00:45:52 -0400

    cambios

--- src/app.js
+++ src/app.js
@@ -4,11 +4,11 @@ import adminRoutes from "./routes/admin.routes.js"
 import indexRoutes from "./routes/index.routes.js";
 import "./config.js";
 import cors from "cors";
-import { CORS } from "./config.js";
+import {  DB_CORS } from "./config.js";
 const app = express();
 
 app.use(cors({
-  origin:CORS,
+  origin:DB_CORS,
 }))
 
 app.use(express.json());
