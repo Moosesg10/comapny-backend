d8d2388767209177433212a9c8b6439f5d07df47
Moosesg10 <moisesgak66@gmail.com>
2023-08-12 00:44:39 -0400

    cambios

--- src/config.js
+++ src/config.js
@@ -8,4 +8,4 @@ export const DB_PORT  = process.env.DB_PORT || 3306
 export const DB_USER  = process.env.DB_USER || 'root'
 export const DB_PASSWORD = process.env.DB_PASSWORD || ""
 export const DB_DATABASE = process.env.DB_DATABASE || "companydb"
-export const CORS = process.env.CORS || "http://localhost:8080"
+export const DB_CORS = process.env.CORS 
